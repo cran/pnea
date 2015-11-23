d6fb941f0233d4b669bf8277dbd544eabfa662a9
Mirko Signorelli <m.signorelli@rug.nl>
2015-11-23 12:20:37 +0000

    version 1.2.0

--- R/pnea.R
+++ R/pnea.R
@@ -1,16 +1,34 @@
 ####### INTERNAL FUNCTIONS
 # Create network matrix from adjacency matrix and nodes list
-netfromadj = function(A, names) {
-  nedges = sum(A)
-  ngenes = length(names)
-  out = matrix(nrow=nedges, ncol=2)
-  k=1
-  for (i in 1:(ngenes) ) {
-    for (j in (1:ngenes)[-i] ) {
-      if (A[i,j]==1) {
-        out[k,1]=names[i]
-        out[k,2]=names[j]
-        k=k+1
+netfromadj = function(A, nodes, nettype) {
+  if ((nettype %in% c('directed','undirected')) == FALSE) stop("nettype must be either 'directed' or 'undirected' ")
+  if (nettype == 'directed')  {
+    nedges = sum(A)
+    ngenes = length(nodes)
+    out = matrix(nrow=nedges, ncol=2)
+    k=1
+    for (i in 1:(ngenes) ) {
+      for (j in (1:ngenes)[-i] ) {
+        if (A[i,j]==1) {
+          out[k,1]=nodes[i]
+          out[k,2]=nodes[j]
+          k=k+1
+        }
+      }
+    }
+  }
+  else if (nettype == 'undirected')  {
+    nedges = sum(A)/2
+    ngenes = length(nodes)
+    out = matrix(nrow=nedges, ncol=2)
+    k=1
+    for (i in 2:(ngenes) ) {
+      for (j in 1:(i-1) ) {
+        if (A[i,j]==1) {
+          out[k,1]=nodes[i]
+          out[k,2]=nodes[j]
+          k=k+1
+        }
       }
     }
   }
@@ -18,10 +36,9 @@ netfromadj = function(A, names) {
 }
 
 # compute pvalue
-pvalue = function(naf, nout, oa, i_f) {
-  oac = nout - oa
-  p1 = phyper(naf, m = oa, n = oac, k = i_f, lower.tail = FALSE) # p > naf
-  p2 = phyper(naf-1, m = oa, n = oac, k = i_f, lower.tail = TRUE) # p < naf
+pvalue = function(x, ss, K, N) { #2*min(p>,p<)+p=
+  p1 = phyper(x, m = K, n = N-K, k = ss, lower.tail = FALSE) # p > nab
+  p2 = phyper(x-1, m = K, n = N-K, k = ss, lower.tail = TRUE) # p < nab
   p3 = 1-p1-p2
   pval = 2*min(p1,p2)+p3
   overenr = (p1<p2)
@@ -32,105 +49,162 @@ pvalue = function(naf, nout, oa, i_f) {
 ########## pnea CLASS
 pneac = function(x,...) UseMethod('pnea', x)
 
-pnea = function(agslist, fgslist = NULL, network, nodes, alpha = NULL, agsnames = NULL, fgsnames = NULL) {
-  if (class(network) == "matrix") {
-    if ( ncol(network)>2 ) net = netfromadj(network, nodes)
-    else if ( ncol(network)==2 ) net=network
-  }
-  else if (class(network) == 'igraph') {
+pnea = function(alist, blist = NULL, network, nettype, nodes, alpha = NULL, anames = NULL, bnames = NULL) {
+  if (is.null(alpha) == FALSE) {if ( alpha<=0 | alpha>=1 ) stop('alpha must be in (0,1)')}
+  if ((nettype %in% c('directed','undirected')) == FALSE) stop("nettype must be either 'directed' or 'undirected' ")
+  # first case: igraph object
+  if (class(network) == 'igraph') { 
     requireNamespace("igraph", quietly = TRUE)
     adjacency = as.matrix(igraph::get.adjacency(network))
-    net = netfromadj(adjacency, nodes)
+    if (isSymmetric(adjacency)==TRUE & nettype == 'directed') {
+      warning('The adjacency matrix is symmetric. Should you set nettype = "undirected"?')
+    }
+    else if (isSymmetric(adjacency)==FALSE & nettype == 'undirected') {
+      warning('The adjacency matrix is not symmetric. Should you set nettype = "directed"?')
+    }
+    net = netfromadj(adjacency, nodes, nettype)
   }
+  # second case: adjacency matrix
+  else if (class(network) == "matrix" & ncol(network)>2 ) {
+    if (isSymmetric(network)==TRUE & nettype == 'directed') {
+      warning('The adjacency matrix is symmetric. Should you set nettype = "undirected"?')
+    }
+    if (isSymmetric(network)==FALSE & nettype == 'undirected') {
+      warning('The adjacency matrix is not symmetric. Should you set nettype = "directed"?')
+    }
+    net = netfromadj(network, nodes, nettype)
+  }
+  # third case: two-column matrix with labels
+  else if (class(network) == "matrix" & ncol(network)==2) net=network 
   # NB: from this point on, 'net' is the network matrix to be used!!!
-  if (is.null(alpha) == FALSE) {if ( alpha<=0 | alpha>=1 ) stop('alpha must be in (0,1)')}
   if (is.factor(nodes) == T) {nodes = as.character(nodes)}
-  oa = numeric()
-  i_f = numeric()
-  agslogic = vector("list", length(agslist)) 
-  netred = vector("list", length(agslist)) # rows where the first column entry is in A
-  fgslogic = vector("list", length(fgslist))
-  naf = numeric()
-  p = naf
-  expect = naf
-  o = vector()
-  concl = o
-  from = o
-  to = o
-  if ( is.null( names(agslist) ) ) names(agslist) = agsnames
-  for (i in 1:length(agslist)) {
-    if (is.factor(agslist[[i]]) == T) {agslist[[i]] = as.character(agslist[[i]])}
-    agslogic[[i]] = (net[,1] %in% agslist[[i]])
-    oa[i] = sum(agslogic[[i]])
-    netred[[i]] = net[agslogic[[i]],]
-  }
-  # first case: each ags vs each fgs (fgslist provided!)
-  if (is.null(fgslist) == FALSE) {
-    if ( is.null( names(fgslist) ) ) names(fgslist) = fgsnames
-    for (i in 1:length(fgslist)) {
-      if (is.factor(fgslist[[i]]) == T) {fgslist[[i]] = as.character(fgslist[[i]])}
-      fgslogic[[i]] = (net[,2] %in% fgslist[[i]])
-      i_f[i] = sum(fgslogic[[i]])
+  oa = numeric(); ib = numeric(); nab = numeric() 
+  alogic = vector("list", length(alist))
+  netred = vector("list", length(alist))
+  p = numeric(); expect=numeric()
+  o = vector(); concl = vector(); from = vector(); to = vector()
+  # UNDIRECTED NETWORKS:
+  if (nettype == 'undirected') {
+    eina = vector("list", length(alist))
+    alogic2 = alogic
+    if ( is.null( names(alist) ) ) names(alist) = anames
+    for (i in 1:length(alist)) {
+      if (is.factor(alist[[i]]) == T) {alist[[i]] = as.character(alist[[i]])}
+      eina[[i]] = (net[,1] %in% alist[[i]])|(net[,2] %in% alist[[i]])
+      netred[[i]] = net[eina[[i]],]
+      alogic[[i]] = (netred[[i]][,1] %in% alist[[i]])
+      alogic2[[i]] = (netred[[i]][,2] %in% alist[[i]])
+      oa[i] = sum(alogic[[i]])+sum(alogic2[[i]]) # total degree of A (undirected net!)
     }
-    nout = dim(net)[1]
-    k=1
-    for (i in 1:length(agslist)) {
-      for (j in 1:length(fgslist)) {
-        from[k] = names(agslist)[i]
-        to[k] = names(fgslist)[j]
-        naf[k] = sum( netred[[i]][,2] %in%  fgslist[[j]])
-        temp = pvalue(naf[k], nout = nout, oa = oa[i], i_f[j])
-        p[k] = temp$p
-        expect[k] = round(i_f[j] * oa[i] / nout, 4)
-        o[k] = temp$overenr
-        if (is.null(alpha) == FALSE) {
-          if (p[k] > alpha) concl[k] = 'No enrichment'
-          else if (p[k] <= alpha & o[k]==T) concl[k] = 'Overenrichment'
-          else if (p[k] <= alpha & o[k]==F) concl[k] = 'Underenrichment'
+    # first case: each A vs each B (blist provided!)
+    if (is.null(blist) == FALSE) {
+      blogic = vector("list", length(blist))
+      blogic2 = blogic
+      einb = vector("list", length(blist))
+      if ( is.null( names(blist) ) ) names(blist) = bnames
+      for (i in 1:length(blist)) {
+        if (is.factor(blist[[i]]) == T) {blist[[i]] = as.character(blist[[i]])}
+        einb[[i]] = (net[,1] %in% blist[[i]])|(net[,2] %in% blist[[i]])
+        blogic[[i]] = (net[einb[[i]],][,1] %in% blist[[i]])
+        blogic2[[i]] = (net[einb[[i]],][,2] %in% blist[[i]])
+        ib[i] = sum(blogic[[i]]) + sum(blogic2[[i]]) # total degree of B (undirected net!)
+      }
+      k=1
+      for (i in 1:length(alist)) {
+        for (j in 1:length(blist)) {
+          from[k] = names(alist)[i]
+          to[k] = names(blist)[j]
+          link = (alogic[[i]] & netred[[i]][,2] %in% blist[[j]])|(alogic2[[i]] & netred[[i]][,1] %in% blist[[j]])
+          nab[k] = sum(link)
+          temp = pvalue(nab[k], ss = oa[i], K = ib[j], N = 2*dim(net)[1])
+          p[k] = temp$p
+          expect[k] = round(ib[j] * oa[i] / (2*dim(net)[1]), 4)
+          o[k] = temp$overenr
+          if (is.null(alpha) == FALSE) {
+            if (p[k] > alpha) concl[k] = 'No enrichment'
+            else if (p[k] <= alpha & o[k]==T) concl[k] = 'Overenrichment'
+            else if (p[k] <= alpha & o[k]==F) concl[k] = 'Underenrichment'
+          }
+          k = k+1
         }
-        k = k+1
       }
     }
-  }
-  # second case: only agslist provided
-  if (is.null(fgslist) == TRUE) {
-    for (i in 1:length(agslist)) {
-      fgslogic[[i]] = (net[,2] %in% agslist[[i]])
-      i_f[i] = sum(fgslogic[[i]])
-    }
-    nout = dim(net)[1]
-    k=1
-    for (i in 1:(length(agslist)-1)) {
-      for (j in (i+1):length(agslist)) {
-        from[k] = names(agslist)[i]
-        to[k] = names(agslist)[j]
-        naf[k] = sum( netred[[i]][,2] %in%  agslist[[j]])
-        temp = pvalue(naf[k], nout = nout, oa = oa[i], i_f[j])
-        p[k] = temp$p
-        expect[k] = round(i_f[j] * oa[i] / nout, 4)
-        o[k] = temp$overenr
-        if (is.null(alpha) == FALSE) {
-          if (p[k] > alpha) concl[k] = 'No enrichment'
-          else if (p[k] <= alpha & o[k]==T) concl[k] = 'Overenrichment'
-          else if (p[k] <= alpha & o[k]==F) concl[k] = 'Underenrichment'
+    # second case: blist NOT provided
+    if (is.null(blist) == TRUE) {
+      ib = oa # undirected: A = B (and no distinction between indegree and outdegree)
+      k=1
+      for (i in 1:(length(alist)-1)) {
+        for (j in (i+1):length(alist)) {
+          from[k] = names(alist)[i]
+          to[k] = names(alist)[j]
+          link = (alogic[[i]] & netred[[i]][,2] %in% alist[[j]])|(alogic2[[i]] & netred[[i]][,1] %in% alist[[j]])
+          nab[k] = sum(link)
+          temp = pvalue(nab[k], ss = oa[i], K = ib[j], N = 2*dim(net)[1])
+          p[k] = temp$p
+          expect[k] = round(ib[j] * oa[i] / (2*dim(net)[1]), 4)
+          o[k] = temp$overenr
+          if (is.null(alpha) == FALSE) {
+            if (p[k] > alpha) concl[k] = 'No enrichment'
+            else if (p[k] <= alpha & o[k]==T) concl[k] = 'Overenrichment'
+            else if (p[k] <= alpha & o[k]==F) concl[k] = 'Underenrichment'
+          }
+          k = k+1
         }
-        k = k+1
       }
     }
-  }
+  } # end for part for undirected networks
+  # DIRECTED NETWORKS:
+  if (nettype == 'directed') {
+    if ( is.null( names(alist) ) ) names(alist) = anames
+    for (i in 1:length(alist)) {
+      if (is.factor(alist[[i]]) == T) {alist[[i]] = as.character(alist[[i]])}
+      alogic[[i]] = (net[,1] %in% alist[[i]])
+      oa[i] = sum(alogic[[i]]) # outdegree of A
+      netred[[i]] = net[alogic[[i]],]
+    }
+    # directed: only relevant case is each A vs B (blist provided!)
+    if (is.null(blist) == FALSE) {
+      blogic = vector("list", length(blist))
+      if ( is.null( names(blist) ) ) names(blist) = bnames
+      for (i in 1:length(blist)) {
+        if (is.factor(blist[[i]]) == T) {blist[[i]] = as.character(blist[[i]])}
+        blogic[[i]] = (net[,2] %in% blist[[i]])
+        ib[i] = sum(blogic[[i]]) # indegree of B
+      }
+      k=1
+      for (i in 1:length(alist)) {
+        for (j in 1:length(blist)) {
+          from[k] = names(alist)[i]
+          to[k] = names(blist)[j]
+          nab[k] = sum( netred[[i]][,2] %in%  blist[[j]])
+          temp = pvalue(nab[k], ss = oa[i], K = ib[j], N = dim(net)[1])
+          p[k] = temp$p
+          expect[k] = round(ib[j] * oa[i] / dim(net)[1], 4)
+          o[k] = temp$overenr
+          if (is.null(alpha) == FALSE) {
+            if (p[k] > alpha) concl[k] = 'No enrichment'
+            else if (p[k] <= alpha & o[k]==T) concl[k] = 'Overenrichment'
+            else if (p[k] <= alpha & o[k]==F) concl[k] = 'Underenrichment'
+          }
+          k = k+1
+        }
+      }
+    }
+  } # end of part for directed networks
   # final common code
   if (is.null(alpha) == FALSE) {
-    results = data.frame(from, to, naf, expect, p, concl)
-    names(results) = c('AGS', 'FGS', 'naf', 'expected_naf', 'pvalue', 'conclusion')
+    results = data.frame(from, to, nab, expect, p, concl)
+    names(results) = c('A', 'B', 'nab', 'expected_nab', 'pvalue', 'conclusion')
   }
   else {
-    results = data.frame(from, to, naf, expect, p)
-    names(results) = c('AGS', 'FGS', 'naf', 'expected_naf', 'pvalue')
+    results = data.frame(from, to, nab, expect, p)
+    names(results) = c('A', 'B', 'nab', 'expected_nab', 'pvalue')
   }
   class(results)=c('pnea','data.frame')
   results
 }
 
+# summary, plot and print:
 summary.pnea = function(object, ...) {
   cat("Number of comparisons:",dim(object)[1],"\n")
   cat("Enrichments at 1% level:",sum(object$pvalue<0.01),"\n")
@@ -141,7 +215,7 @@ summary.pnea = function(object, ...) {
 plot.pnea = function(x, nbreaks=10, ...) {
   par(mfrow=c(1,2))
   hist(x$pvalue,breaks=nbreaks, xlim=c(0,1), xlab='p-value', main='Histogram of p-values')
-  plot(ecdf(x$p),main='P-p plot of p-values',xlab='x',ylab ='F(x)',xlim=c(0.037,0.963),ylim=c(0.037,0.963))
+  plot(ecdf(x$pvalue),main='P-p plot of p-values',xlab='x',ylab ='F(x)',xlim=c(0.037,0.963),ylim=c(0.037,0.963))
   abline(v=1);abline(a=0,b=1,col='red')
   legend(0.6,0.2,c('Distribution of','p-values','Uniform','distribution'),col=c('black','white','red','white'),lty=1,cex=0.6)
   }
